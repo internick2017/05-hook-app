0c0a7568048cddc9e8b6c2e11667eec97ab37253
Nick Granados <internick2017@gmail.com>
2022-02-09 21:41:32 -0300

    use Counter -custom hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { HookApp } from './HookApp';
 
 
@@ -8,7 +9,7 @@ import { CounterApp } from './components/01-useState/CounterApp';
 
 ReactDOM.render(
 
-  <CounterApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 
 );
