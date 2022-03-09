6f1e53b8156d983d106597533f113965ab5f277d
Nick Granados <internick2017@gmail.com>
2022-03-08 21:58:18 -0300

    inicio useConetxt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
@@ -20,7 +21,7 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 
 ReactDOM.render(
 
-    <TodoApp />,
+    <MainApp />,
     document.getElementById('root')
 
 );
